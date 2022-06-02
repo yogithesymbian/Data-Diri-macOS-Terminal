44b68735f92ec4299d2127dd6e61d2b5f69be927
yogithesymbian <yogirenbox33@gmail.com>
2022-06-03 06:32:14 +0800

    first

--- Data Diri/main.swift
+++ Data Diri/main.swift
@@ -7,5 +7,29 @@
 
 import Foundation
 
-print("Hello, World!")
+// MARK: Membuat form data diri
+//print("Selamat Datang di Dicoding Academy")
+//let firstName = "Yogi", lastName = "Arif Widodo"
+//let fullName = firstName + " " + lastName
+//let address = "Samarinda"
+//let job = "iOS Developer"
+//let age = 0b10001
 
+print("Masukkan nama depan Anda:"); let firstName = readLine()!
+print("Masukkan nama belakang Anda:")
+let lastName = readLine()!
+print("Masukkan alamat Anda:"); let address = readLine()!
+print("Masukkan job Anda:"); let job = readLine()!
+print("Masukkan umur Anda:"); let age = readLine()!
+
+// MARK: Menggabungkan nama depan dan belakang
+let fullName = firstName + " " + lastName
+
+print("--------------------------------------")
+
+// MARK: Mencetak program yang anda buat ke dalam terminal
+print("Apakah kalian tahu \(fullName)?")
+print("\(firstName) adalah seorang \(job)")
+print("Saat ini ia berumur \(age) dan bertempat tinggal di \(address).")
+
+print("--------------------------------------")
